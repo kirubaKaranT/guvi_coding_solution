79e11365ed5e9ee6ca0cf57bde6eb11e3ec333b2
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-15 08:37:45 +0530

    Create pow.java

---
+++ pow.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class pow
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+	    int n=in.nextInt();
+	    int m=in.nextInt();
+	   System.out.print((int)Math.pow(n,m));
+	}
+}
