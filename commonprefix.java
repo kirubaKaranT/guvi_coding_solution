88fd6a99d6ac58f3388f2e98539c91885995363f
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-17 00:06:01 +0530

    Update commonprefix.java

--- commonprefix.java
+++ commonprefix.java
@@ -1,17 +1,26 @@
+/* package whatever; // don't place package name! */
+
 import java.util.*;
-public class Main
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+/* Name of the class has to be "Main" only if the class is public. */
+class commonprefix
 {
+	
+
   public static void main (String[]args)
   {
+        String res="";
     Scanner in = new Scanner (System.in);
     int n=in.nextInt();
     String[] arr=new String[n];
-     in = new Scanner (System.in);
+     //in = new Scanner (System.in);
     for(int i=0;i<n;i++){
-        arr[i]=in.nextLine();
-         in = new Scanner (System.in);
+        arr[i]=in.next();
+       //  in = new Scanner (System.in);
     }
-    String res="";
+
     for(int i=0;i<arr[0].length();i++){
         char c=arr[0].charAt(i);
         int j=0;
