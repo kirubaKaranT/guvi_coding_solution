7bd84314bf3170279c9b8bbbf4d4d12040df17f3
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-16 22:03:05 +0530

    Create bin.java

---
+++ bin.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		n=(int)Math.pow(2,n);
+		for(int i=0;i<n;i++){
+		    int temp=i;
+		    String res="";
+		    int t=(int)Math.sqrt(n);
+		    while(t-->=0){
+		        if((temp&1)==1){
+		            res="1"+res;		        }
+		        else{
+		            res="0"+res;
+		        }
+		        temp=temp>>1;
+		    }
+		    System.out.println(res);
+		    
+		}
+	}
+}
