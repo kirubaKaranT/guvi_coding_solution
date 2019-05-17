ab49c08e2aed36c59b460d356253efd4e0ef5f1a
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-17 07:45:43 +0530

    Create single.java

---
+++ single.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	boolean kk=true;
+	int n=in.nextInt();
+    int[] arr=new int[n];
+    
+    for(int i=0;i<n;i++)
+    arr[i]=in.nextInt();
+    
+    Arrays.sort(arr);
+    
+    for(int i=0;i<n;i+=2){
+        if(arr[i]!=arr[i+1]){
+        System.out.print(arr[i]);
+        break;
+            
+        }
+    }
+	}
+}
