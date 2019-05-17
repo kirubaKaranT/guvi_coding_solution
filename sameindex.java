63fa0ea892873eebfe1d4435a0fd4a761139845f
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-17 07:38:17 +0530

    Create sameindex.java

---
+++ sameindex.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+    int[] arr=new int[n];
+    
+    for(int i=0;i<n;i++)
+    arr[i]=in.nextInt();
+    
+    for(int i=0;i<n;i++){
+        if(i==arr[i]){
+            System.out.print(i+" ");
+        }
+    }
+	    
+	}
+}
