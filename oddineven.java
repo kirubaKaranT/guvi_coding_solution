5a64ee58253b2a197fabe8e3ffc820d1db1a8081
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-17 08:10:04 +0530

    Update oddineven.java

--- oddineven.java
+++ oddineven.java
@@ -15,13 +15,13 @@ public class Main
 	for(int i=0;i<n;i++){
 	if(i%2==0){
 	    if(arr[i]%2==1){
-	        System.out.print(arr[i]+" ");
+	        System.out.println(arr[i]);
 	    }
 	}
 	else{
 	    
 	    if(arr[i]%2==0){
-	        System.out.print(arr[i]+" ");
+	        System.out.println(arr[i]);
 	    }
 	}
 	}
