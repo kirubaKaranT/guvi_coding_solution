15e74e93b8e2c3f2822426e10fe1ba6f336a33d3
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-20 17:43:46 +0530

    Update hun120.java

--- hun120.java
+++ hun120.java
@@ -8,7 +8,7 @@ public class Main
 	for(int i=0;i<n;i++){
 	    arr[i]=in.nextInt();
 	}
-	int kk=0;
+	int kk=1;
 	for(int i=0;i<n-2;i++){
 	    for(int j=i+1;j<n-1;j++){
 	        for(int k=j+1;k<n-1;k++){
