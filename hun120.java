c205b700ac4e1a3c7e522ac40bbc1f05777ec229
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-20 17:41:46 +0530

    Update hun120.java

--- hun120.java
+++ hun120.java
@@ -11,7 +11,7 @@ public class Main
 	int kk=0;
 	for(int i=0;i<n-2;i++){
 	    for(int j=i+1;j<n-1;j++){
-	        for(int k=j+1;k<n;k++){
+	        for(int k=j+1;k<n-1;k++){
 	            if(arr[i]+arr[j]==arr[k]){
                      kk=arr[k];
 	            }
