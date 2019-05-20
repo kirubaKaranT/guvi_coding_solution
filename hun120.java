50178cd7e995b8f367389052df73565bd7a22ea4
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-20 17:40:12 +0530

    Create hun120.java

---
+++ hun120.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+	int[] arr=new int[n];
+	for(int i=0;i<n;i++){
+	    arr[i]=in.nextInt();
+	}
+	int kk=0;
+	for(int i=0;i<n-2;i++){
+	    for(int j=i+1;j<n-1;j++){
+	        for(int k=j+1;k<n;k++){
+	            if(arr[i]+arr[j]==arr[k]){
+                     kk=arr[k];
+	            }
+	        }
+	    }
+	}
+	
+   System.out.print(kk);
+	}
+}
