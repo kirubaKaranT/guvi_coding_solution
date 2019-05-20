f7b7467eba1782c3fe1451e542c65663718e5bbe
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-20 16:32:33 +0530

    Create diasum.java

---
+++ diasum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int[][] arr=new int[n][n];
+		
+		for(int i=0;i<n;i++){
+		    for(int j=0;j<n;j++){
+		           arr[i][j]=in.nextInt(); 
+		    }
+		}
+		int sum=0;
+		for(int i=0;i<n;i++){
+		    sum+=arr[i][i];
+		}
+		System.out.print(sum);
+	}
+}
