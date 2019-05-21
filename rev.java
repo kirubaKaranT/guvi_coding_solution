c28fb7c84441a39083530a27c96c27afb575af6d
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-21 12:07:30 +0530

    Update rev.java

--- rev.java
+++ rev.java
@@ -4,9 +4,9 @@ public class Main
 {
 	public static void main(String[] args) {
     Scanner in=new Scanner(System.in);
-    
-	    String n=in.nextLine();
-	    String res="";
+    	int ki=in.nextInt();
+	    String n=ki+"";
+	String res="";
 	    for(int i=n.length()-1;i>=0;i--){
 	        res+=n.charAt(i);
 	    }
