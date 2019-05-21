f95a57c790899b99cb187c938e314d664f0ddc9f
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-21 12:08:11 +0530

    Create revnum.java

---
+++ revnum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    	int ki=in.nextInt();
+	    String n=ki+"";
+	String res="";
+	    for(int i=n.length()-1;i>=0;i--){
+	        res+=n.charAt(i);
+	    }
+	    System.out.print(res);
+	}
+}
