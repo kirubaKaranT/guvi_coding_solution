953733795aa71e13549a9a212569cdbefca82d0e
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-21 17:48:02 +0530

    Update rightan.java

--- rightan.java
+++ rightan.java
@@ -6,8 +6,12 @@ public class Main
 	int n=in.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n-i+1;j++){
-                System.out.print("1 ");
-            }
+                System.out.print("1");
+			if(j<=n-i){
+				System.out.print(" ");
+			}
+	    }
+		
             
             System.out.print("\n");
         }
