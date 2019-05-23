dfc49662bbd24b73719bcc32050e47c302096463
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-23 17:35:51 +0530

    Create findmaxpath.java

---
+++ findmaxpath.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main
+{  static int max=1;    
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int[][] path=new int[n+1][n+1];
+		int edge=in.nextInt();
+		while(edge-->0){
+		    int x=in.nextInt();
+		    int y=in.nextInt();
+		    path[x][y]=1;
+		}
+		
+		find(1,path,n,1);
+		System.out.print(max);
+		
+	}
+public static void 	find(int current,int[][] path, int n,int cost){
+	    for(int next=1;next<=n;next++ ){
+	       if(cost>max){
+	           max=cost;
+	       }
+	        if(path[current][next]==1){
+	            find(next,path,n,cost+next);
+	        }
+	    }
+	}
+}
