9b655b0e6855800267c341ef41faf6b049988923
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-23 14:20:48 +0530

    Create sumton.java

---
+++ sumton.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    int n=in.nextInt();
+	    System.out.print((n*(n+1)/2));
+	
+	}
+}
