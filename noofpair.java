4d55fa8ab0ea00f65aea2dc482d3e3add0c396a4
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-28 12:02:43 +0530

    Update noofpair.java

--- noofpair.java
+++ noofpair.java
@@ -11,7 +11,10 @@ public class Main
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		int n=in.nextInt();
-		System.out.print(n-(int)(Math.pow(2,(Math.log(n)/Math.log(2)))));
+		if(Math.ceil((Math.log(n)/Math.log(2)))==Math.floor(Math.log(n)/Math.log(2)))
+		System.out.print(0);
+		else
+		System.out.print((int)(n-Math.pow(2,Math.floor(Math.log(n)/Math.log(2)))));
 		
 	}
 }
