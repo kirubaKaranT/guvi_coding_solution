d06f3a483b19e4c75eae41099ce6d8603d05aeae
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-29 21:38:29 +0530

    Rename noname.java to substr.java

---
+++ substr.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.*;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String s=in.next();
+        int n=in.nextInt();
+        for(int i=0;i<=s.length()-n;i++){
+            System.out.print(s.substring(9,i+n)+" ");
+        }
+	}
+
+}
