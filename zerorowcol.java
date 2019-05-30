ec8f212b3c15928fa41a09e31dd3c0f27dd3c0c8
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-05-30 11:41:09 +0530

    Update zerorowcol.java

--- zerorowcol.java
+++ zerorowcol.java
@@ -1,9 +1,16 @@
-import java.util.*;
+/* package whatever; // don't place package name! */
 
-public class Main {
-  
-  public static void main(String[] args) {
-    Scanner in=new Scanner(System.in);
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
     int n=in.nextInt();
     int m=in.nextInt();
     HashSet<Integer> zerorow=new HashSet<Integer>();
@@ -31,9 +38,13 @@ public class Main {
     }
     for(int i=0;i<n;i++){
       for(int j=0;j<m;j++){
+      	if(j!=m-1)
         System.out.print(arr[i][j]+" ");
+		else{
+			System.out.print(arr[i][j]);
+		}
       }
       System.out.println();
       }
-  }
+	}
 }
