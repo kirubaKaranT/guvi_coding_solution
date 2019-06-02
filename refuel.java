c7e60d53d14f878474a776b6a20118709af4a260
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-02 16:04:25 +0530

    Create refuel.java

---
+++ refuel.java
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int dis=in.nextInt();
+    int fuel=in.nextInt();
+    int station=in.nextInt();
+    int n=in.nextInt();
+   
+    int refuel=0;
+    int tank=fuel;
+    Boolean forward=true;
+    fuel=fuel-station;
+    while(n-->0){
+      if(forward){
+       
+       if(2*(dis-station)>fuel){
+         fuel=tank;
+         if(2*(dis-station)>fuel){
+           refuel=-1;
+           break;
+         }
+
+         refuel++;
+       }
+       fuel=fuel-(2*(dis-station));
+        forward=false;
+      }
+      else{
+        
+        if(2*station>fuel){
+          fuel=tank;
+          if(2*station>fuel){
+            refuel=-1;
+            break;
+          }
+          refuel++;
+        }
+       fuel=fuel-2*station;
+       
+        forward=true;
+      }
+    }
+		System.out.print(refuel);
+	}
+}
