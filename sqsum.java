f45c86d5667b761566f8d1eb55f7505d49c65eed
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-03 18:46:28 +0530

    Create sqsum.java

---
+++ sqsum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int k=n;
+		int temp=0;
+		while(n>0){
+		    int rem=n%10;
+		    n/=10;
+		    temp+=Math.pow(rem,2);
+		}
+		
+		    System.out.print(temp);
+		
+	}
+}
