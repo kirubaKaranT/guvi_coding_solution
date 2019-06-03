d4efcda99710d32fbf45cab521b7b07e2e26dead
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-03 23:08:54 +0530

    Update sumpow4.java

--- sumpow4.java
+++ sumpow4.java
@@ -4,11 +4,12 @@ public class Main
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		int n=in.nextInt();
+    int len=(n+"").length();
 		int temp=0;
 		while(n>0){
 		    int rem=n%10;
 		    n/=10;
-		    temp+=Math.pow(rem,4);
+		    temp+=Math.pow(rem,len);
 		}
 		
 		    System.out.print(temp);
