9d939b0fbffc48f96cdb026133e5e2325a4ac677
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-05 18:21:36 +0530

    Create nextbig.java

---
+++ nextbig.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    boolean not=true;
+    int max=(int)Math.pow(10,(n+"").length()+1);
+    for(int i=n+1;i<max;i++){
+      if(isana(i+"",n+"")){
+        System.out.print(i);
+        not=false;
+        break;
+      }
+    }
+      if(not){
+        System.out.print("impossible");
+      }
+
+ 
+  }
+  public static boolean isana(String d,String s){
+    boolean anagram=true;
+	    HashSet<Character> hash =new HashSet<Character>();
+	    for(Character c:d.toCharArray()){
+	        hash.add(c);
+	    }
+	        if(d.length()!=s.length())
+            return false;
+            else{
+                for(Character c:s.toCharArray()){
+                    if(!hash.contains(c)){
+                        anagram=false;
+                    }
+                }
+            }
+                if(anagram){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+  }
+}
