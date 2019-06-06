83056336f7bd1b14e4487de9f40bee4ee4ad3789
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-06 11:19:37 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=in.nextInt();
+    }
+    Arrays.sort(arr);
+    for(int i=0;i<n;i++){
+    if(i!=n-1)
+    System.out.print(arr[i]+" ");
+    else
+    System.out.print(arr[i]);
+    }
+  }
+}
