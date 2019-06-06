1c0aefd0da93967606e16bccfebe7ff43523fc7e
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-06 19:22:50 +0530

    Update maxdays.java

--- maxdays.java
+++ maxdays.java
@@ -5,16 +5,18 @@ public class Main {
   public static void main(String[] args) {
     Scanner in=new Scanner(System.in);
     int n=in.nextInt();
-    int work=in.nextInt();
+    int readtime=in.nextInt();
     int maxread=0;
     for(int i=0;i<n;i++){
-      maxread+=in.nextInt();
+      int work=in.nextInt();
+      int rem=86400-work;
+      if(readtime>0){
+        readtime-=rem;
+        maxread++;
+      }
+      
     }
-    maxread+=work;
-    int q=maxread/86400;
-    if(maxread%86400!=0){
-      q+=1;
-    }
-    System.out.print(q);
+    
+    System.out.print(maxread);
   }
 }
