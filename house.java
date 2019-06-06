27675b98bb84de218a1f843d391cd6ab132554d8
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-06 18:20:00 +0530

    Create house.java

---
+++ house.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int k=in.nextInt();
+    int ava=n-k;
+    int pos=2*k;
+    int max=Math.min(ava,pos);
+    System.out.println(1+" "+max);
+  
+  }
+}
