faf7e0dfbb2b46dce0297346ea48f8704451cce5
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-07 16:29:34 +0530

    Create hun69.java

---
+++ hun69.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    String s=in.next();
+    boolean kk=true;
+    int at=0,dot=0;
+    for(int i=0;i<s.length();i++){
+      if(s.charAt(i)=='@'){
+        at++;
+      }
+      if(s.charAt(i)=='.'){
+        dot++;
+      }
+    }
+
+    if(s.contains("@")&&s.contains(".")){
+      
+      if(at==1&&dot==1&&s.substring(s.indexOf('@'),s.indexOf('.')+1).length()>=4&&s.substring(0,s.indexOf('@')).length()>=3&&".com".equals(s.substring(s.indexOf('.')))){
+        System.out.print("YES");
+        kk=false;
+      }
+    }
+    if(kk){
+      System.out.print("NO");
+    }
+  }
+}
