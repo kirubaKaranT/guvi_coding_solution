f952b41f88d2dfecf7162e8075d0769f01376a7f
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-09 10:20:41 +0530

    Create p14.java

---
+++ p14.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    String s=in.next();
+    for(int i=s.length()-1;i>=0;i--){
+      char c=s.charAt(i);
+      if(c!='a'&&c!='e'&&c!='i'&&c!='o'&&c!='u'){
+        System.out.print(c);
+      }
+    }
+    
+    
+  }
+}
