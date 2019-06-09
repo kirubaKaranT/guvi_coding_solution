1a6d5c53756b0b664dee18dece27c28e2f0122eb
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-09 17:25:54 +0530

    Update hun106.java

--- hun106.java
+++ hun106.java
@@ -9,14 +9,14 @@ public class Main {
     String b=in.nextLine();
     int i=0;
     for(i=0;i<a.length()&&i<b.length();i++){
-      System.out.println(b.charAt(i)+" "+a.charAt(i));
+      System.out.println(b.charAt(i)+""+a.charAt(i));
     }
     while(i<a.length()){
-      System.out.println("  "+a.charAt(i));
+      System.out.println(" "+a.charAt(i));
       i++;
     }
     while(i<b.length()){
-      System.out.println(b.charAt(i)+"  ");
+      System.out.println(b.charAt(i)+" ");
       i++;
     }
   }
