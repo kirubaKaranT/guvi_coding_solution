35c170533b5e0347c3f6dd44c756c180fc987025
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-09 09:45:42 +0530

    Create p11.java

---
+++ p11.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    String a=in.next();
+      if(a.equals("Sunday")){
+        System.out.print("yes");
+      }
+      else{
+        System.out.print("no");
+      }
+    
+    
+  }
+}
