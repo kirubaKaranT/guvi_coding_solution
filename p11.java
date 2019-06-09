5443a9e614d6372aa93ee718e0541b0f787a0ca7
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-09 09:47:08 +0530

    Update p11.java

--- p11.java
+++ p11.java
@@ -5,7 +5,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner in=new Scanner(System.in);
     String a=in.next();
-      if(a.equals("Sunday")){
+      if(a.equals("Sunday")||a.equals("Saturday")){
         System.out.print("yes");
       }
       else{
