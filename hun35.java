fb69bb7037582010e4c52c8e2e6ee7e695ebbd0e
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-10 14:22:34 +0530

    Update hun35.java

--- hun35.java
+++ hun35.java
@@ -4,17 +4,8 @@ public class Main
 	public static void main(String[] args) {
 		Scanner in =new Scanner(System.in);
     String s=in.next();
-    int[] arr=new int[256];
-    for(Character c:s.toCharArray()){
-      arr[c]++;
-    }
-    int c=0;
-    for(int i=0;i<arr.length;i++){
-      if(arr[i]%2!=0){
-        c++;
-      }
-    }
-    if(c==1)
+    int n=s.length();
+    if(n%2!=0&&s.substring(0,n/2).equals(s.substring((n/2)+1)))
     System.out.print("YES");
     else
     {
