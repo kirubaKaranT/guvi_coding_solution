f1a577f284359cc3fc97de800faff922dbbbd4fa
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-10 15:10:53 +0530

    Create hun78.java

---
+++ hun78.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main
+{
+  static int height=0;
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+    int node=in.nextInt();
+    int e=in.nextInt();
+    int[][] path=new int[e][2];
+    while(e-->0){
+      path[e][0]=in.nextInt();
+      path[e][1]=in.nextInt();
+
+    }
+    find(path,1,path[path.length-1][0]);
+    System.out.print(height);
+	}
+  public static void find(int[][] path,int h,int l){
+    
+    for(int i=0;i<path.length;i++){
+      if(path[i][0]==l){  
+       // System.out.print(path[i][0]+" "+path[i][1]);
+        find(path,h+1,path[i][1]);
+      }
+    }
+    if((h)>height){
+      height=h;
+    }
+  }
+  	
+}
