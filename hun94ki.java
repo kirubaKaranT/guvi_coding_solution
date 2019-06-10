b3c9ad561d26530beaf665fcb7ff3fe4aa2bfc20
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-10 13:17:31 +0530

    Update hun94ki.java

--- hun94ki.java
+++ hun94ki.java
@@ -3,11 +3,10 @@ public class Main
 {
 	public static void main(String[] args) {
 		Scanner in =new Scanner(System.in);
-	String kk="";
-	  //  while(in.hasNext())
+		String kk="";
+	 
 		kk=in.nextLine();
-	
-		//System.out.print(kk);
+		
 		String res="",temp="";
 		for(int i=0;i<kk.length();i++){
         if(kk.charAt(i)!=' '){
