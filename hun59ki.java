7bd026bb126a453e13bf73f13c0a48bad17cb620
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-13 09:11:38 +0530

    Create hun59ki.java

---
+++ hun59ki.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int[] a=new int[n];
+    int[] b=new int[n];
+ 
+    for(int i=0;i<n;i++){
+      a[i]=in.nextInt();
+    }
+    
+    for(int i=0;i<n;i++){
+      b[i]=in.nextInt();
+    }
+    for(int i=0;i<n;i++){
+     System.out.print((a[i]+b[i])+" ");
+    }
+    
+
+      
+  }
+}
