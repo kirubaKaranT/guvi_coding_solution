c81e1bb047d08cc6b663da8fda275a15a9198b28
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-15 07:46:15 +0530

    Create pl23.java

---
+++ pl23.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int x1=in.nextInt();
+    int y1=in.nextInt();
+    int x2=in.nextInt(); 
+    int y2=in.nextInt();
+    int x3=in.nextInt();
+    int y3=in.nextInt();
+    if((x1==x2&&x2==x3)||(y1==y2&&y2==y3)) 
+    System.out.print("yes");
+    else
+    System.out.print("no");
+  
+  }
+}
