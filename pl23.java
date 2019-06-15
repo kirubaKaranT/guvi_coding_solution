b5248cef54f431efb64dac12ac4d2e7a1ccd476d
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-15 07:55:28 +0530

    Update pl23.java

--- pl23.java
+++ pl23.java
@@ -10,7 +10,9 @@ public class Main {
     int y2=in.nextInt();
     int x3=in.nextInt();
     int y3=in.nextInt();
-    if((x1==x2&&x2==x3)||(y1==y2&&y2==y3)) 
+    if(x1==y1&&x2==y2&&x3==y3)
+    System.out.print("yes");
+    else if((x1==x2&&x2==x3)||(y1==y2&&y2==y3)) 
     System.out.print("yes");
     else
     System.out.print("no");
