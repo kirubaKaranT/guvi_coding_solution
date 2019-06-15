52969e3c01de81a25d1c89185d0ee382e3710508
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-15 08:28:46 +0530

    Create ki48.java

---
+++ ki48.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    String a=in.next();
+    String b=in.next();
+    for(int i=0;i<a.length();i++){
+      for(int j=0;j<b.length();j++){
+        if(a.charAt(i)==b.charAt(j)){
+          boolean sub=true;
+          int k=i;
+          int l=j;
+          while(k<a.length()&&l<b.length()){
+              if(a.charAt(i)!=b.charAt(j)){
+                sub=false;
+              }
+              k++;
+              l++;
+          }
+          if(sub){
+            System.out.print(i);
+            System.exit(0);        
+          }
+        }
+      }
+    }
+    
+      System.out.print(-1);
+  }
+}
