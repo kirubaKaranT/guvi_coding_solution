f7006bf43a06eb542a8ee833394e9558b65ad277
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-15 19:59:08 +0530

    Create ki32.java

---
+++ ki32.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    HashSet<Integer> hash=new HashSet<Integer>();
+    int n=in.nextInt();
+    int k=in.nextInt();
+    for(int i=0;i<n;i++){
+      hash.add(in.nextInt());
+    }
+    if(hash.contains(k)){
+      System.out.print("yes");
+    }
+    else{
+      System.out.print("no");
+    }
+
+    
+   
+    
+  }
+  
+}
