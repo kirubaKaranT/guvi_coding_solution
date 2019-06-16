3b83c5973a8d2b902c2c5f6309b84ef040b38db5
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-16 10:01:15 +0530

    Update ki201.java

--- ki201.java
+++ ki201.java
@@ -5,8 +5,20 @@ public class Main {
   public static void main(String[] args) {
     Scanner in=new Scanner(System.in);
     int n=in.nextInt();
-    for(int i=0;i<=n;i++)
-    System.out.print(catalan(i)+" ");
+    if(n==0)
+		{
+			System.out.print(1);
+		}
+		else{
+		for (int i = 0; i < n; i++) 
+		{ 
+			System.out.print(catalan(i));
+				if(i<n-1)
+				{
+					System.out.print(" ");
+				}
+		}
+		}
       
   }
  public static  int catalan(int n) { 
