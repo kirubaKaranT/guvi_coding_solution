d273a06b0f86d4acff591190cb23c22d1e7eec0d
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-16 13:30:46 +0530

    Create ki52.java

---
+++ ki52.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int k=in.nextInt();
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=in.nextInt();
+    }
+    Arrays.sort(arr);
+      System.out.print(arr[k-1]);
+      
+  }
+}
