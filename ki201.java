7e62edc21522adbd60759863b8da55ab8dde35fc
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-16 09:56:48 +0530

    Create ki201.java

---
+++ ki201.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    for(int i=0;i<=n;i++)
+    System.out.print(catalan(i)+" ");
+      
+  }
+ public static  int catalan(int n) { 
+        int res = 0; 
+          
+        // Base case 
+        if (n <= 1) { 
+            return 1; 
+        } 
+        for (int i = 0; i < n; i++) { 
+            res += catalan(i) * catalan(n - i - 1); 
+        } 
+        return res; 
+    } 
+}
