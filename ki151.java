91b122616d2f89abb3a3bfeaf211364d115d2c77
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-16 13:21:53 +0530

    Create ki151.java

---
+++ ki151.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.regex.*;
+class Main {
+  public static void main(String[] args) {
+   Scanner in=new Scanner(System.in);
+   String s=in.next();
+   int c=0;
+   for(int i=0;i<s.length();i++){
+   if(Pattern.matches("[a|b]*", s.charAt(i)+"")){
+     
+   }
+   else{
+     c++;
+   }
+   }
+   if(c==0||c==1){
+     System.out.print("yes");
+   }
+   else{
+     System.out.print("no");
+   }
+  }
+}
