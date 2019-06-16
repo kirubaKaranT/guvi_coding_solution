01e25182491befdb26e26856e88845f19b69776b
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-16 19:56:39 +0530

    Create ki66.java

---
+++ ki66.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+   int k=in.nextInt();
+   int[] arr=new int[9999999];
+    for(int i=0;i<n;i++){
+    arr[in.nextInt()]++;
+    }
+
+    for(int i=0;i<arr.length;i++){
+    
+    if(arr[i]==k){
+      System.out.print(i+" ");
+    }
+
+    }
+  }
+}
