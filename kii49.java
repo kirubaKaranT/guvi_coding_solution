de85c2c47c181cd670a71504c881a61b82af2a0b
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-16 19:23:58 +0530

    Update kii49.java

--- kii49.java
+++ kii49.java
@@ -8,8 +8,11 @@ public class Main {
     if(n>=Integer.MIN_VALUE&&n<=Integer.MAX_VALUE){
       System.out.print("INT");
     }
+    else if(n>=(-Math.pow(2,31)+1)&&n<=(Math.pow(2,31)+1)){
+      System.out.print("LONG INT");
+    }
     else{
-      System.out.print("LONG");
+      System.out.print("LONG LONG");
     }
   }
 }
