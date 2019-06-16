eed64bbd4cbb6cbcf298e74866835f6454fbac35
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-16 12:47:36 +0530

    Update ki117.java

--- ki117.java
+++ ki117.java
@@ -6,8 +6,8 @@ public class Main {
   public static void main(String[] args) {
     Scanner in=new Scanner(System.in);
     String  a=in.next();
-    for(int i=0;i<a.length();i++){
-      if(i!=a.length()-1)
+    for(int i=a.length()-1;i>=0;i--){
+      if(i!=0)
       System.out.print(a.charAt(i)+"-");
       else{
         System.out.print(a.charAt(i));
