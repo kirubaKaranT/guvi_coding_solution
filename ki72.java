a96470ce9a8d976dd0946403183abd216fc4063e
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-16 20:19:19 +0530

    Create ki72.java

---
+++ ki72.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int[] arr=new int[n];
+    int max=0;
+    for(int i=0;i<n;i++){
+      arr[i]=in.nextInt();
+    }
+    Arrays.sort(arr);
+
+    System.out.print(arr[arr.length-1]);
+    
+      
+    
+    
+  }
+}
