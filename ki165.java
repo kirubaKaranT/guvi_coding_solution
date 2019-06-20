226910385fa45ab1d83f88352a55694ac783383c
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-20 22:57:29 +0530

    Create ki165.java

---
+++ ki165.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int k=in.nextInt();
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+    arr[i]=in.nextInt();
+    }
+    Arrays.sort(arr);
+  int min=0;
+    for(int i=0;i<n;i++){
+      if(arr[i]>k){
+          min=arr[i];
+          break;
+      }
+    }
+    
+    System.out.print(min);
+    
+      
+  }
+}
