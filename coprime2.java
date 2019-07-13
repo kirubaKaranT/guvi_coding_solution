d02b95a0a4133e6bb5ece60bba08978e8eb80780
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-07-13 20:00:44 +0530

    Create coprime2.java

---
+++ coprime2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+    int a=in.next().length();
+    int b=in.next().length();
+		if(a+1==b)
+		System.out.print("yes");
+    else
+    System.out.print("no");
+	}
+  	
+}
